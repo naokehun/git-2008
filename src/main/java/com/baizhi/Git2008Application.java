a2401ee5fd519130bb85091913ee05eb704c3cfd
weiqin <2535232239@qq.com>
2021-01-29 16:39:41 +0800

    git-2008项目基础环境v1.0

---
+++ src/main/java/com/baizhi/Git2008Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git2008Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Git2008Application.class, args);
+    }
+
+}
